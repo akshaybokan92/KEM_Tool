a3feae56fa8e82d7ceeda0818a3ddd968e867c03
Akshay.Bokan <Akshay.Bokan@BSL-BNG-L103.betsol.com>
2018-02-12 16:44:27 +0530

    kemLogin changes

--- KEM_Tool/src/com/kem/ddt/Kemlogin.java
+++ KEM_Tool/src/com/kem/ddt/Kemlogin.java
@@ -1,16 +1,17 @@
 package com.kem.ddt;
 
+import java.lang.System.Logger;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.kem.resource.Kem_LoginPage;
 import com.kem.utility.ExcelDataConfig;
 
 public class Kemlogin {
@@ -20,22 +21,29 @@ public class Kemlogin {
 	@Test(dataProvider="KemLoginData" )
 	public void launchbrowser(String username, String password) throws InterruptedException {
 		
-		//driver path
-		System.setProperty("webdriver.chrome.driver","D:\\Eclipse Automation\\chromedriver.exe");
+		//Driver path
+		System.setProperty("webdriver.chrome.driver","./Configuration/chromedriver.exe");
 		
-		//launch browser
+		//Launch browser
+		
 		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
 		driver.get("http://35.164.157.128/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
 		
-		//test case steps
+		//Test case steps
+		
 		driver.findElement(By.id("username")).sendKeys(username);
 		driver.findElement(By.id("password")).click();
+		//Kem_LoginPage.login.txt_password(driver).click();
 		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.id("login-btn")).click();
-		// verification point
+		Thread.sleep(2000);
+		
+	
+		
+		// Verification Point
 		Assert.assertTrue(driver.getTitle().contains("KEM"), "login failed");
 		System.out.println("Login successful");
 
@@ -51,7 +59,7 @@ public class Kemlogin {
 	public Object[][] passData()
 	{
 		
-		ExcelDataConfig config = new ExcelDataConfig("E:\\LoginDetails.xlsx");
+		ExcelDataConfig config = new ExcelDataConfig("./testdata/Users.xlsx");
 		int rows = config.getRowCount(0);
 		Object [][] data = new Object [rows][2];
 		try{
